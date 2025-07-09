82ac90c13daaf1c19455b6e77ae0002d49413c9b
Fukulyn <a098838628@gmail.com>
2025-07-09 20:39:09 +0800

    Update

--- RaspberryPI/Backend/Archive/routes/feeder_route.py
+++ RaspberryPI/Backend/Archive/routes/feeder_route.py
@@ -3,7 +3,6 @@ from controllers.feeder_controller import *
 
 feeder_bp = Blueprint('feeder', __name__)
 feeder_bp.route('/feed', methods=['POST'])(feed_once)
-feeder_bp.route('/weight', methods=['GET'])(read_weight)
-feeder_bp.route('/distance', methods=['GET'])(read_distance)
+feeder_bp.route('/status', methods=['GET'])(broadcast_status_loop)
 feeder_bp.route('/waste/open', methods=['POST'])(open_waste)
 feeder_bp.route('/waste/close', methods=['POST'])(close_waste)
